288a0f13450f963740f97a9b8f0c3396a2454b7b
Mena Wahba <menawahba@gmail.com>
2021-02-14 21:02:54 -0500

    added ajax function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// console.log("hello")
+var APIKey ="6b9b6924ca7bdd8131872c6e5a9fa3ec";
+var city = "East Brunswick";
+var savedCity = [""];
+
+// var clearHistory = $("#clearHistory");
+// var searchButton = $("#searchButton");
+var searchedCity = $("#searchCity");
+
+
+function todaysWeather(){
+    var todaysURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:todaysURL,
+        method:"GET",
+    }).then(function(response){
+        console.log(response);
+    });
+}
